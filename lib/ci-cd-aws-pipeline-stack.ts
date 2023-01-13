39fc4fb74d2c6fdab4109782c8fedad92f1410bf
Sahil Gupta <guptasahil2810@gmail.com>
2023-01-13 23:19:18 +0530

    "initial commit"

--- lib/ci-cd-aws-pipeline-stack.ts
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdpipeline from 'aws-cdk-lib/pipelines';
+import { CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdpipeline.CodePipeline(this, 'Pipeline', {
+      pipelineName: "CDK Test Pipeline",
+      synth : new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('SahilGupta2810/ci-cd-aws-pipeline','release-1'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
