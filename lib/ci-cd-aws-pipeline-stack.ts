b915eb62554adee5a4d20d238731db347aeb62c1
Sahil Gupta <guptasahil2810@gmail.com>
2023-01-14 00:42:16 +0530

    "Initialize Lambda"

--- lib/ci-cd-aws-pipeline-stack.ts
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -2,13 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdpipeline from 'aws-cdk-lib/pipelines';
 import { CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new cdpipeline.CodePipeline(this, 'Pipeline', {
+    const pipeline = new cdpipeline.CodePipeline(this, 'Pipeline', {
       pipelineName: "Test_Pipeline_CDK",
       synth : new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('SahilGupta2810/ci-cd-aws-pipeline','release-1'),
@@ -19,5 +20,10 @@ export class CiCdAwsPipelineStack extends cdk.Stack {
         ]
       })
     })
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test"));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod"));
+
   }
 }
